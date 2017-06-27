26c873bc607b578b7231f0b07596e53543af9773
gotpop <liamsobczak@gmail.com>
2017-06-27 14:40:04 +0100

    Added model

---
+++ src/app/shared/collectable.service.ts
@@ -0,0 +1,6 @@
+export class CollectableService {
+  private collectables = [
+    { description: 'Lorem ipsum dolor sit amet, consectetur', type: 'book' },
+    { description: 'Lorem ipsum dolor sit amet, consectetur', type: 'book' }
+  ]
+}
