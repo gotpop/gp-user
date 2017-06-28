e69d72760d06f171015e44c95ca50d786045eca4
gotpop <liamsobczak@gmail.com>
2017-06-28 10:58:26 +0100

    Added subpage basic markup

--- src/app/step2/step2.component.ts
+++ src/app/step2/step2.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/user.service'
 
 @Component({
   selector: 'app-step2',
-  templateUrl: './step2.component.html',
-  styles: []
+  templateUrl: './step2.component.html'
 })
 export class Step2Component implements OnInit {
 
-  constructor() { }
+  submitted = false;
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+
+  get diagnostic() { return JSON.stringify(this.model); }
+
+
+  constructor(private userStore: UserService) { }
+
+  model = this.userStore.makeModel()
+
+  onSubmit() {
+    this.submitted = true;
+    this.userStore.setValue(this.model)
+    let checkit = this.userStore.getValue()
+    console.log(checkit)
+  }
 
   ngOnInit() {
   }
-
 }
--- src/app/step3/step3.component.ts
+++ src/app/step3/step3.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/user.service'
 
 @Component({
   selector: 'app-step3',
-  templateUrl: './step3.component.html',
-  styles: []
+  templateUrl: './step3.component.html'
 })
 export class Step3Component implements OnInit {
 
-  constructor() { }
+  submitted = false;
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+
+  get diagnostic() { return JSON.stringify(this.model); }
+
+
+  constructor(private userStore: UserService) { }
+
+  model = this.userStore.makeModel()
+
+  onSubmit() {
+    this.submitted = true;
+    this.userStore.setValue(this.model)
+    let checkit = this.userStore.getValue()
+    console.log(checkit)
+  }
 
   ngOnInit() {
   }
